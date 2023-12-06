f9d5e5b2160f34354261737a647a8b09d2161d03
Jonatan Torino <Jonatan.Torino@hotmail.com>
2023-12-06 13:20:28 +0000

    Primera version de backup, con param opc

---
+++ BackupSqlDB.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+param (
+    [string]
+    $ExtraDescription
+    ,
+    [string]
+    $Database = "AxDB"
+)
+
+$backupfile = "J:\MSSQL_BACKUP\$Database-$env:computername-$(Get-Date -format "yyyyMMdd-HHmm")"
+if ([string]::IsNullOrEmpty($ExtraDescription) ){
+    $backupfile += ".bak"
+}
+else {
+    $backupfile += "_$ExtraDescription.bak"
+}
+Write-Host -ForegroundColor Green $backupfile
+Backup-SqlDatabase -ServerInstance "localhost" -Database $Database -BackupFile $backupfile -CompressionOption On
