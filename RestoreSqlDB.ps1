f6dcbf2c0606fdb5c79b96433ba287d6c149cf12
Jonatan Torino <jonatan.torino@hotmail.com>
2024-01-22 13:34:30 +0000

    RestoreSqlDB

--- RestoreSqlDB.ps1
+++ RestoreSqlDB.ps1
@@ -8,6 +8,9 @@ param (
     [string]
     $Database = "AxDB"
 )
+# Import-Module -Name SQLPS
+Import-Module -Name SqlServer
+
 # Configuración
 $serverInstance = "localhost"  # Reemplaza con tu nombre de instancia
 
