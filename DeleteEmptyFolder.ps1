889e50b25d46144712f9fcc5c9b26b74297e1bf6
Jonatan Torino <jonatan.torino@hotmail.com>
2024-12-06 14:15:14 +0000

    Fix en DeleteEmptyFolder

--- DeleteEmptyFolder.ps1
+++ DeleteEmptyFolder.ps1
@@ -14,23 +14,23 @@ param (
 # Get hidden files or not. Depending on removeHiddenFiles setting
 $getHiddelFiles = !$removeHiddenFiles
 # Remove empty directories locally
-# Function Delete-EmptyFolder($path)
-# {
+Function DeleteEmptyFoldersAndSubFolders($path) 
+{
     # Go through each subfolder, 
     Foreach ($subFolder in Get-ChildItem -Force -Literal $path -Directory) 
     {
         # Call the function recursively
-        Delete-EmptyFolder -path $subFolder.FullName
+        DeleteEmptyFoldersAndSubFolders -path $subFolder.FullName
     }
     # Get all child items
     $subItems = Get-ChildItem -Force:$getHiddelFiles -LiteralPath $path
     # If there are no items, then we can delete the folder
     # Exluce folder: If (($subItems -eq $null) -and (-Not($path.contains("DfsrPrivate")))) 
-    If ($subItems -eq $null) 
+    If ($null -eq $subItems) 
     {
         Write-Host "Removing empty folder '${path}'"
         Remove-Item -Force -Recurse:$removeHiddenFiles -LiteralPath $Path -WhatIf:$whatIf
     }
-# }
+}
 # Run the script
-# Delete-EmptyFolder -path "C:\enter\your\path\here"
+DeleteEmptyFoldersAndSubFolders -path $folderPath
